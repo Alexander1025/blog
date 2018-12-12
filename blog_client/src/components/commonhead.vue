aa661c8f6af28bec6ffe41c2a43881197b48976e
756418095@qq.com <756418095@qq.com>
2018-12-12 16:24:51 +0800

    添加并使用vuex

--- blog_client/src/components/commonhead.vue
+++ blog_client/src/components/commonhead.vue
@@ -3,28 +3,18 @@
         <div class="mainwrap mainhead">
             <img class="logoimg" src="./../static/images/common/logo.png" alt="机智僧">
                 <ul class="headtag" v-if="!isshowheadtag">
-                    <li class="active">首页</li>
-                    <li>前端</li>
-                    <li>应用</li>
-                    <li>标签</li>
-                    <li>杂谈</li>
-                    <li>留言</li>
-                    <li>音乐</li>
-                    <li>关于</li>
+                    <li v-for="(item, index) in headtagarr" :class="[item.isactive?'active':'']">
+                        {{item.cont}}
+                    </li>
                 </ul>
             <div class="headtoggle" @click="headtoggle">
                 <img src="./../static/images/icon/Hamburger_white.png" alt="汉堡包">
             </div>
             <transition name="headtagphone">
                 <ul class="headtagphone headtag" v-if="isshowheadtag">
-                    <li class="active">首页</li>
-                    <li>前端</li>
-                    <li>应用</li>
-                    <li>标签</li>
-                    <li>杂谈</li>
-                    <li>留言</li>
-                    <li>音乐</li>
-                    <li>关于</li>
+                    <li v-for="(item, index) in headtagarr" :class="[item.isactive?'active':'']">
+                        {{item.cont}}
+                    </li>
                 </ul>
             </transition>
         </div>
@@ -38,6 +28,44 @@ export default {
     data () {
         return {
             isshowheadtag: false,
+            headtagarr: [
+                {
+                    isactive: true,
+                    cont: "首页",
+                },
+                {
+                    isactive: false,
+                    cont: "前端",
+                },
+                {
+                    isactive: false,
+                    cont: "应用",
+                },
+                {
+                    isactive: false,
+                    cont: "标签",
+                },
+                {
+                    isactive: false,
+                    cont: "杂谈",
+                },
+                {
+                    isactive: false,
+                    cont: "留言",
+                },
+                {
+                    isactive: false,
+                    cont: "音乐",
+                },
+                {
+                    isactive: false,
+                    cont: "关于",
+                },
+                // {
+                //     isactive: false,
+                //     cont: this.$store.state.test
+                // }
+            ],
         }
     },
     components:{
---
+++ blog_client/src/store/store.js
@@ -0,0 +1,80 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {trim,myparse} from './../static/js/common.js'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        test:"test",
+        datemonth: "",
+        username: "hi",
+        islogin:false,
+        footerindex:1,
+        theme:[0,"",""], //主题, (0-背景颜色, 1-图片), 头部样式， 主体样式
+        setnowdate:new Date(), //设置时间不设置默认当前时间 ，new Date().getTime()+86400000*10
+    },
+    mutations: {
+        getMonth:function (store,mon){
+            let monarr = ['一','二','三','四','五','六','七','八','九','十','十一','十二',];
+            store.datemonth = monarr[mon];
+        },
+        setState:function (store,option){
+            store[option.attr] = option.field;
+        },
+    },
+    actions: {
+        getUserMsg (store,mon) {
+
+            var that = this;
+            var ajax = new XMLHttpRequest();
+            ajax.open('post','/node/getuser');
+            ajax.send();
+            ajax.onreadystatechange = function () {
+                if (ajax.readyState==4 &&ajax.status==200) {
+                    var data = ajax.responseText;
+                    data = myparse(data);
+                    // console.log(data);//输入相应的内容
+                    if(data.status == 1){
+                        store.commit('setState',{attr:'username',field:data.data[0].username});
+                        store.commit('setState',{attr:'islogin',field:true});
+                        // console.log(data.data[0].theme);
+                        if(data.data[0].theme){
+                            let themeArr = data.data[0].theme.split(",");
+                            store.commit('setState',{attr:'theme',field:[themeArr[0],themeArr[1],themeArr.slice(2).join(",")]});
+                        }
+                    }else if(data.status == 0){
+                        store.commit('setState',{attr:'username',field:'请登录'});
+                        store.commit('setState',{attr:'islogin',field:false});
+                    }
+                }
+            }
+        },
+        logout (store){
+            var that = this;
+            var ajax = new XMLHttpRequest();
+            ajax.open('post','/node/logout');
+            ajax.send();
+            ajax.onreadystatechange = function () {
+                if (ajax.readyState==4 &&ajax.status==200) {
+                    var data = ajax.responseText;
+                    data = myparse(data);
+                    // console.log(data);//输入相应的内容
+                    if(data.status == 1){
+                        layer.open({
+                            content: "已安全登出",
+                            skin: 'msg',
+                            time: 2,
+                        });
+                        store.commit('setState',{attr:'username',field:'请登录'});
+                        store.commit('setState',{attr:'islogin',field:false});
+                        store.commit('setState',{attr:'theme',field:[0,"",""]});
+                    }
+                }
+            }
+
+        }
+    }
+})
+
+export default store
